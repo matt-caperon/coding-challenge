315982621d584913e52b4a9a9bbf9123ddaa52e3
Matt Caperon <matthew.caperon@outlook.com>
2019-09-14 11:05:19 +0100

    Added Ship.cs

---
+++ CodingChallenge/Ship.cs
@@ -0,0 +1,18 @@
+﻿
+namespace CodingChallenge
+{
+    enum Orientation{
+        North = 'N', 
+        East = 'E',
+        South = 'S',
+        West = 'W'
+    }
+    
+    class Ship
+    {
+        public bool IsLost { get; set; }
+        public int XCoordinate { get; set; }
+        public int YCoordinate { get; set; }
+        public Orientation Orientation { get; set; }
+    }
+}
